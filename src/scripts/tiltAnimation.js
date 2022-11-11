86946fe6aa4ff42a031a03977e1705052d79ab51
Gabriel Berberian <gabrielberberian0@gmail.com>
2022-11-11 00:57:09 -0300

    arreglando el vanilla tilt

--- src/scripts/tiltAnimation.js
+++ src/scripts/tiltAnimation.js
@@ -1,5 +1,4 @@
-import VanillaTilt from 'vanilla-tilt';
-VanillaTilt.init(this.$refs.refToDiv);
+import VanillaTilt from "../../node_modules/vanilla-tilt/"
 
 export default function initTiltAnimation() {
   const elements = document.querySelectorAll(".js-tilt");
